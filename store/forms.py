d53f0794fcd4bd1052f37c0b9a50a7dd86242e65
Radwa246 <radwaemaddd2003@gmail.com>
2025-05-17 22:22:54 +0500

    Update forms.py

--- store/forms.py
+++ store/forms.py
@@ -3,6 +3,6 @@
 from django import forms
 
 class CheckoutForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Mike')
-    email = forms.EmailField(label='saqibkhank805@gmail.com')
+    name = forms.CharField(max_length=100, label='Your name: ')
+    email = forms.EmailField(label='Your email: ')
     address = forms.CharField(widget=forms.Textarea, label='Shipping Address')
