5cc32a0db48a2395c2a261987b188d504c2263d5
Radwa246 <radwaemaddd2003@gmail.com>
2025-05-17 22:11:56 +0500

    Update settings.py

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -129,4 +129,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mikeschulte990@gmail.com'        # Replace this
-EMAIL_HOST_PASSWORD = 'quju cxof pkfa otwc'     # Replace this
+EMAIL_HOST_PASSWORD = 'veje dvck mmyz ypgr'     # Replace this
